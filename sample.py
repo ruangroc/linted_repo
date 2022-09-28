2697170c82d3906eb7b22e835014b1f11796773a
Chalida (Anita) Ruangrotsakun <ruangroc@oregonstate.edu>
2022-09-28 09:13:50 -0700

    fifth precommit hook test

--- sample.py
+++ sample.py
@@ -10,18 +10,4 @@ a = [
     "i-fruit",
 ]
 
-print("more poor formatting here blah blah formatting linting lint linter")
-
-a = [
-    "apple",
-    "banana",
-    "coconut",
-    "dragonfruit",
-    "e-fruit",
-    "fig",
-    "grapes",
-    "h-fruit",
-    "i-fruit",
-]
-
 a = ['apple', 'banana', 'coconut', 'dragonfruit', 'e-fruit', 'fig', 'grapes', 'h-fruit', 'i-fruit']
