4b0485e8489f0f2577ea123503e10bd90c3dc4b4
Chalida (Anita) Ruangrotsakun <ruangroc@oregonstate.edu>
2022-09-28 09:09:15 -0700

    testing pre commit hook again

--- sample.py
+++ sample.py
@@ -1,4 +1,15 @@
-a = ['apple', 'banana', 'coconut', 'dragonfruit', 'e-fruit', 'fig', 'grapes', 'h-fruit', 'i-fruit']
+a = [
+    "apple",
+    "banana",
+    "coconut",
+    "dragonfruit",
+    "e-fruit",
+    "fig",
+    "grapes",
+    "h-fruit",
+    "i-fruit",
+]
 
 print("more poor formatting here blah blah formatting linting lint linter")
 
+a = ['apple', 'banana', 'coconut', 'dragonfruit', 'e-fruit', 'fig', 'grapes', 'h-fruit', 'i-fruit']
