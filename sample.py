b449d36d19329461e55680c636129d0bf731a1e5
Chalida (Anita) Ruangrotsakun <ruangroc@oregonstate.edu>
2022-09-28 09:10:50 -0700

    third precommit hook test

--- sample.py
+++ sample.py
@@ -12,4 +12,16 @@ a = [
 
 print("more poor formatting here blah blah formatting linting lint linter")
 
+a = [
+    "apple",
+    "banana",
+    "coconut",
+    "dragonfruit",
+    "e-fruit",
+    "fig",
+    "grapes",
+    "h-fruit",
+    "i-fruit",
+]
+
 a = ['apple', 'banana', 'coconut', 'dragonfruit', 'e-fruit', 'fig', 'grapes', 'h-fruit', 'i-fruit']
