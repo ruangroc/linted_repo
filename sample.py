40b5cf30779beeb3c6e8413d79da5141e740ca73
Chalida (Anita) Ruangrotsakun <ruangroc@oregonstate.edu>
2022-09-28 18:58:14 -0700

    testing precommit hook

--- sample.py
+++ sample.py
@@ -12,14 +12,4 @@ a = [
     "i-fruit",
 ]
 
-a = [
-    "apple",
-    "banana",
-    "coconut",
-    "dragonfruit",
-    "e-fruit",
-    "fig",
-    "grapes",
-    "h-fruit",
-    "i-fruit",
-]
+a = ["apple", "banana", "coconut", "dragonfruit", "e-fruit", "fig", "grapes", "h-fruit", "i-fruit"]
